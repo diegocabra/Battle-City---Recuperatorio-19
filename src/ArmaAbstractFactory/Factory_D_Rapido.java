529f512a45a50a59b15a59ad96dc151f18ef1deb
DgoCabra <Diego.m876@gmail.com>
2020-04-03 20:18:54 -0300

    Agregue Enunciado

--- src/ArmaAbstractFactory/Factory_D_Rapido.java
+++ src/ArmaAbstractFactory/Factory_D_Rapido.java
@@ -6,12 +6,9 @@ import Disparo.DisparoEnemigo;
 
 public class Factory_D_Rapido extends Arma{
 
-	
-	
-
-public Factory_D_Rapido(int daño) {
-	super(daño);
-}
+		public Factory_D_Rapido(int daño) {
+			super(daño);
+		}
 		
 		public Disparo CrearDisparo() {
 			return new DisparoEnemigo(daño);
